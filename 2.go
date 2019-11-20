6c7101730ebe29ecd7668abb049bd1ed6fcece2b
Sergio <52253089+DurdenTyler2008@users.noreply.github.com>
2019-11-20 11:54:27 +0300

    простейшие операции

---
+++ 2.go
@@ -0,0 +1,21 @@
+//простейшие операции
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("10+2=", 10+2)
+	fmt.Println("10*2=", 10*2)
+	fmt.Println("10.0/3.0=", 10.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+// golang
+//10+2= 12
+//10*2= 20
+//10.0/3.0= 3.3333333333333335
+//false
+//true
+//false
